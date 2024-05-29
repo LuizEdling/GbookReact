19935e4349e59d6caad6ff968cb0be5c3068d3af
Felipe Gorgo Kiçula <felipegorgo25@gmail.com>
2024-05-29 16:59:19 -0300

    Components Header e Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './components/Header/index';
+import Footer from './components/Footer/index';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      teste
+      <Footer />
     </div>
   );
 }
